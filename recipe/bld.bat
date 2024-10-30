0d4f0d34e9bcc7e04dd1c91de6d049d6d2649a86
Jon Mease <jonmmease@gmail.com>
2024-10-30 17:49:29 -0400

    libprotobuf :facepalm:

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,4 +1,3 @@
-set PROTOC=%BUILD_PREFIX%\Library\bin\protoc.exe
 cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
 %PYTHON% -m pip install . -vv
 if errorlevel 1 exit 1
--- recipe/build.sh
+++ recipe/build.sh
@@ -1,3 +1,2 @@
-export PROTOC=${BUILD_PREFIX}/bin/protoc
 cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
 $PYTHON -m pip install . -vv
